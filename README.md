0ef784db52bfa0a0bc428f77383a848c81a24e72
djenidisimple <djenidigauss@gmail.com>
2026-06-06 10:41:44 +0300

    add circular linked list

--- README.md
+++ README.md
@@ -14,3 +14,11 @@ On a une liste qui contient des éléments dont ils stockent
 une valeur puis le pointeur du prochain liste et le pointeur du liste
 qui est devant lui.
 ```
+
+### Liste chaînée Circulaire
+
+```bash
+On a pu voir dans les liste chaînées simple que chaque élément contient le pointeur
+de son prédéceseur et quand c'est le dernier élément il va pointeur vers None ou null
+mais dans liste chaînée circulaire le dernier élément a le pointeur du 1er élément.
+```
---
+++ circular_linked_list.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self, value, next):
+        self.value = value 
+        self.next = next
+
+class CircularLinkedList:
+
+    def __init__(self):
+        self.head = None 
+    
+    def insert_begin(self, value):
+        if self.head is None:
+            self.head = Node(value, None)
+            self.head.next = self.head
+        else:
+            current = self.head
+            while current.next != self.head:
+                current = current.next
+            new_node = Node(value, self.head)
+            current.next = new_node
+            self.head = new_node
+    
+    def insert_end(self, value):
+        if self.head is None:
+            self.head = Node(value, None)
+            self.head.next = self.head
+        else:
+            current = self.head
+            while current.next != self.head:
+                current = current.next
+            current.next = Node(value, self.head)
+    
+    def delete_begin(self):
+        if self.head.next is not None:
+            if self.head.next.next is not None:
+                self.head.next = self.head.next.next
+    
+    def delete_end(self):
+        if self.head is None:
+            return
+        else:
+            current = self.head
+            while current.next.next != self.head:
+                current = current.next
+            current.next = current.next.next
+    
+    def display(self):
+        if self.head is None:
+            return
+        else:
+            current = self.head
+            while current.next != self.head:
+                print(current.value, end=" -> ")
+                current = current.next
+            print(current.value)
+
+linked = CircularLinkedList()
+linked.insert_begin(5)
+linked.insert_begin(8)
+linked.insert_begin(9)
+linked.insert_begin(10)
+# linked.delete_end()
+linked.display()
