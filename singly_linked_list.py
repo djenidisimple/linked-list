7aad4b2f16ab2971fde8eb7360a0b3fb4a964934
djenidisimple <djenidigauss@gmail.com>
2026-06-07 20:41:06 +0300

    add method seach in singlyLinkedList

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -4,10 +4,13 @@ class Node:
         self.value = value
         self.next = next
 class SinglyLinkedList:
+    
     def __init__(self):
         self.head = None
+    
     def insert_begin(self, value):
         self.head = Node(value, self.head)
+
     def insert_end(self, value):
         if self.head is None:
             self.head = Node(value, self.head)
@@ -17,23 +20,41 @@ class SinglyLinkedList:
                 current = current.next
             new = Node(value, None)
             current.next = new
+    
     def display(self):
         current = self.head
         while current.next is not None:
             print(current.value, end=" -> ")
             current = current.next    
         print(current.value)
+    
     def delete_begin(self):
         self.head = self.head.next 
+    
     def delete_end(self):
         current = self.head
         while current.next.next is not None:
             current = current.next
         current.next = None
+
+    def search(self, target):
+        if self.head is None:
+            return False
+        current = self.head
+        while current.next is None:
+            if current.value == target:
+                break
+            else:
+                current = current.next
+        return current.value == target
+        
+
 linked = SinglyLinkedList()
+print("Existe : ", linked.search(5))
 linked.insert_end(5)
 linked.insert_end(8)
 linked.insert_end(9)
 linked.insert_end(10)
 linked.delete_end()
+print("Existe : ", linked.search(5))
 linked.display()
